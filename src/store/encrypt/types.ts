5980952a6f10fdf86d6d5e47e1c987071c4dca23
Denis Stasyev <denisstasyev@gmail.com>
2020-02-24 13:29:51 +0300

    Improve Send

--- src/store/encrypt/types.ts
+++ src/store/encrypt/types.ts
@@ -1,3 +1,8 @@
+interface Error {
+	name: string;
+	message: string;
+}
+
 export interface CipherState {
 	method: string;
 	error: Error;
@@ -11,17 +16,12 @@ export const SET_CIPHER_METHOD = 'SET_CIPHER_METHOD';
 export const SET_CIPHER_KEY = 'SET_CIPHER_KEY';
 export const SET_CIPHER_TEXT = 'SET_CIPHER_TEXT';
 export const SET_CIPHER_CIPHERDATA = 'SET_CIPHER_CIPHERDATA';
+export const SET_CIPHER_ERROR = 'SET_CIPHER_ERROR';
 export const RESET_CIPHER = 'RESET_CIPHER';
-export const OCCUR_CIPHER_ERROR = 'OCCUR_CIPHER_ERROR';
 
 interface SetMethodAction {
 	type: typeof SET_CIPHER_METHOD;
 	method: string;
-}
-
-interface OccurErrorAction {
-	type: typeof OCCUR_CIPHER_ERROR;
-	error: Error;
 }
 
 interface SetKeyAction {
@@ -40,6 +40,11 @@ interface SetCipherDataAction {
 	cipherText: string;
 }
 
+interface SetErrorAction {
+	type: typeof SET_CIPHER_ERROR;
+	error: Error;
+}
+
 interface ResetAction {
 	type: typeof RESET_CIPHER;
 }
@@ -49,5 +54,5 @@ export type CipherActionTypes =
 	| SetKeyAction
 	| SetTextAction
 	| SetCipherDataAction
-	| ResetAction
-	| OccurErrorAction;
+	| SetErrorAction
+	| ResetAction;
