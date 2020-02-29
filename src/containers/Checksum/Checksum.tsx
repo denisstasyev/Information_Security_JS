01eb72b4ae095cb884ea1bea1c22b456b47a6dac
Denis Stasyev <denisstasyev@gmail.com>
2020-03-01 01:08:13 +0300

    Add CRC16, CRC24, CRC32

--- src/containers/Checksum/Checksum.tsx
+++ src/containers/Checksum/Checksum.tsx
@@ -1,5 +1,65 @@
 import * as React from 'react';
 
+import { checksumMethods } from '../../methods'; //TODO: fix bug with absolut imports with Typescript
+import { countChecksum } from '../../methods/checksum';
+
 export default function() {
-  return <>Soon</>;
+  const [method, setMethod] = React.useState(checksumMethods[0]);
+  const [text, setText] = React.useState('');
+  const [error, setError] = React.useState('');
+  const [checksum, setChecksum] = React.useState('');
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    setError('');
+
+    if (text === '') {
+      setError('Введите текст для которого необходимо найти контрольную сумму!');
+      return;
+    }
+
+    setChecksum(countChecksum(method, text));
+  };
+
+  return (
+    <>
+      <h2>Контрольная сумма</h2>
+      <div>1) Выберите метод нахождения контрольной суммы:</div>
+      <select
+        value={method.type}
+        onChange={(event: any) =>
+          setMethod(
+            checksumMethods.find(method => method.type === event.target.value) ||
+              checksumMethods[0],
+          )
+        }
+      >
+        {checksumMethods.map((method, index) => (
+          <option value={method.type} key={index}>
+            {method.name}
+          </option>
+        ))}
+      </select>
+      <div>2) Введите текст для которого требуется найти контрольную сумму:</div>
+      <input
+        value={text}
+        type="text"
+        placeholder="Ваш текст"
+        onChange={(event: any) => setText(event.target.value)}
+      />
+      <button onClick={onSubmit}>Посчитать контрольную сумму!</button>
+
+      {error ? (
+        <div>Ошибка! {error}</div>
+      ) : (
+        checksum && (
+          <>
+            <h3>Ваш результат</h3>
+            <div>1) Контрольная сумма:</div>
+            <output>{checksum}</output>
+          </>
+        )
+      )}
+    </>
+  );
 }
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+
+declare module 'crc/crc16';
+declare module 'crc/crc24';
+declare module 'crc/crc32';
